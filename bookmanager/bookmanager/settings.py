fabfe0f28c5db2a13279d57ad404b5a034f229e4
Gitmaizialy <maizialy@gmail.com>
2021-12-29 18:29:01 +0800

    站点管理

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -100,8 +100,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# 语言
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
+# 时区
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
