85b90fefe7e0e831323994d9da9bc51ace0cdb0a
Gitmaizialy <maizialy@gmail.com>
2021-12-30 11:16:52 +0800

    模板templates

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -8,6 +8,20 @@ from django.shortcuts import render
 
 from django.http import HttpRequest, HttpResponse
 
+
 # 定义http://localhost:8000/index/ 访问函数
 def index(request):
-    return HttpResponse('ok')
+    # return HttpResponse('ok')
+
+    # 视图返回渲染的模板
+    # render            渲染模板
+    # request,          请求
+    # template_name,    模板名字
+    # context=None,     上下文 将视图views中的数据传递给html模板   模板html采用{{key值}}方式接收views中的值
+
+    # 模拟数据库数据
+    context_value = {
+        'name': '这是模拟数据库里的数据value值'
+    }
+
+    return render(request, 'book/templatesindex.html', context=context_value)
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,10 +51,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'bookmanager.urls'
 
+# 模板相关配置
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [],
+        # 设置系统模板文件存放位置
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
