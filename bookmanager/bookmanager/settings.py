ff798f87330a7d86b422d49ad0f6cef3d2b28555
Gitmaizialy <maizialy@gmail.com>
2021-12-30 11:59:37 +0800

    静态资源设置 访问

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -124,9 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# 静态文件目录
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 系统静态文件路径设置
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
