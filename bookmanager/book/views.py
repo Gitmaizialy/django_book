ef8838bba1ed41b53b07de0dc229426b68887684
Gitmaizialy <maizialy@gmail.com>
2021-12-30 10:34:19 +0800

    路由匹配1

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+1. 接收请求request 即HttpRequest的类对象
+2. 返回的响应HttpResponse
+"""
+
+from django.http import HttpRequest, HttpResponse
+
+# 定义http://localhost:8000/index/ 访问函数
+def index(request):
+    return HttpResponse('ok')
